83755772f257b5b556863937a8ce6c437f904203
JingHai_Lingyun <m56042764@163.com>
2023-11-29 00:13:30 +0800

    Remove CI, add a generator

---
+++ update-time-gen.py
@@ -0,0 +1,15 @@
+import re
+from datetime import datetime
+
+# 1. 读取 index.html 文件内容
+with open('index.html', 'r') as file:
+    content = file.read()
+
+# 2. 查找并替换时间部分
+pattern = r'<p>更新时间：(.*?)</p>'
+current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+updated_content = re.sub(pattern, f'<p>更新时间：{current_time}</p>', content)
+
+# 3. 将更新后的内容写回到 index.html 文件
+with open('index.html', 'w') as file:
+    file.write(updated_content)
