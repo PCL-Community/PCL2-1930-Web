eebce8e5820159179e1cf59d8421f93382501418
JingHai_Lingyun <m56042764@163.com>
2023-12-01 00:08:53 +0800

    附文页面分离出来 主站附文不变

--- update-time-gen.py
+++ update-time-gen.py
@@ -2,7 +2,7 @@ import re
 from datetime import datetime
 
 # 1. 读取 index.html 文件内容
-with open('index.html', 'r') as file:
+with open('index.html', 'r', encoding='utf-8') as file:
     content = file.read()
 
 # 2. 查找并替换时间部分
@@ -11,5 +11,5 @@ current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 updated_content = re.sub(pattern, f'<p>更新时间：{current_time}</p>', content)
 
 # 3. 将更新后的内容写回到 index.html 文件
-with open('index.html', 'w') as file:
+with open('index.html', 'w', encoding='utf-8') as file:
     file.write(updated_content)
