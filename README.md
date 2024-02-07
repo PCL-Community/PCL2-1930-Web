ea27e062b8f85f1157ffbd92aebb4b23a41f7a8d
JingHai-Lingyun <m56042764@163.com>
2024-02-08 00:17:04 +0800

    fix #11

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 # 本期更新内容
 
-#### （更新时间：2024-01-13 09:48:50）
+#### （更新时间：2024-02-08 00:16:24）
 
-1：完成 [#9](https://github.com/lingyunawa/pcl-doc/issues/9) 内容微调
-2：完成 [#9](https://github.com/lingyunawa/pcl-doc/issues/9) 新板块编写
+1：完成 [#11](https://github.com/lingyunawa/pcl-doc/issues/11) 内容同步
+2：完成 [#11](https://github.com/lingyunawa/pcl-doc/issues/11) 新板块编写
 
 </div>
--- static/js/script.js
+++ static/js/script.js
@@ -50,12 +50,21 @@ function setInhelpTrigger(){
     }
 }
 
+// Set All Link&Content same link element's href
+function setLinkContentSameLink(){
+    const linkElements = document.querySelectorAll('a.copyLink');
+    for (let linkElement of linkElements) {
+        linkElement.setAttribute('href', linkElement.innerHTML.toString());
+    }
+}
+
 // Links
 function openGuide(){ window.open('/guide.html') }
 function open2611updateLog(){ window.open('https://www.bilibili.com/read/cv28121157/') }
 function openShimoDoc(){ window.open('https://shimo.im/docs/qKPttVvXKqPD8YDC') }
 function open3rdPartyPage(){ window.open('https://github.com/Hex-Dragon/PCL2/issues?q=label%3A%E7%AC%AC%E4%B8%89%E6%96%B9+is%3Aclosed') }
 function openWikiAnc(){ window.open('https://zh.minecraft.wiki/w/Minecraft_Wiki:%E8%BF%81%E7%A7%BB%E9%80%9A%E7%9F%A5?variant=zh-cn#%E6%88%91%E4%BB%AC%E9%9C%80%E8%A6%81%E4%BD%A0%E7%9A%84%E5%B8%AE%E5%8A%A9%EF%BC%81') }
+function openBBSUpdate(){ window.open('https://t.bilibili.com/891597994798874693') }
 
 // FootMarks
 function jumpToTip(highLightElementId){ 
@@ -99,4 +108,5 @@ window.onload = function(){
     setDiscTrigger();
     setInhelpTrigger();
     setTipUnfocusTrigger();
+    setLinkContentSameLink();
 }
