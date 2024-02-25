dee4f8bcba10ef3d030745efe800bac59dd50f3c
JingHai-Lingyun <m56042764@163.com>
2024-02-25 11:36:35 +0800

    fix #16

--- update-time-gen.py
+++ update-time-gen.py
@@ -10,7 +10,7 @@ with open('index.html', 'r', encoding='utf-8') as file:
 pattern = r'最近更新时间：\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 updated_content = re.sub(pattern, f'最近更新时间：{current_time}', content, count=1)
-print(f'更新时间为：{current_time}')
+print(current_time)
 
 # 3. 将更新后的内容写回到 index.html 文件
 with open('index.html', 'w', encoding='utf-8') as file:
