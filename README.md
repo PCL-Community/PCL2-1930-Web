7f3f707c6864ccc43dd59da256a580d94836946c
JingHai-Lingyun <m56042764@163.com>
2024-03-13 21:47:01 +0800

    fix #17

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 # 本期更新内容
 
-#### （更新时间：2024-03-10 09:24:08）
+#### （更新时间：2024-03-13 21:46:51）
 
-1：完成 [2f91ee3](https://github.com/WForst-Breeze/PCL2-1930/commit/2f91ee365d7b9b75c404cce3143f2c1b39630337)，[fb45ba8](https://github.com/WForst-Breeze/PCL2-1930/commit/fb45ba86ddd159f426e6ff380133388690c16454) 内容同步
+1：完成 [#17](https://github.com/lingyunawa/pcl-doc/issues/17) 内容同步
 
 </div>
--- update-time-gen.py
+++ update-time-gen.py
@@ -2,16 +2,18 @@
 import re
 from datetime import datetime
 
-# 1. 读取 index.html 文件内容
-with open('index.html', 'r', encoding='utf-8') as file:
-    content = file.read()
+def replace_time(file_path):
+    # 1. 读取 index.html 文件内容
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    # 2. 查找并替换时间部分
+    pattern = r'更新时间：\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    updated_content = re.sub(pattern, f'更新时间：{current_time}', content, count=1)
+    # 3. 将更新后的内容写回到 index.html 文件
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(updated_content)
+    print(f'Replace time "{current_time}" in file "{file_path}" done')
 
-# 2. 查找并替换时间部分
-pattern = r'最近更新时间：\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'
-current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-updated_content = re.sub(pattern, f'最近更新时间：{current_time}', content, count=1)
-print(current_time)
-
-# 3. 将更新后的内容写回到 index.html 文件
-with open('index.html', 'w', encoding='utf-8') as file:
-    file.write(updated_content)
+replace_time('index.html')
+replace_time('README.md')
