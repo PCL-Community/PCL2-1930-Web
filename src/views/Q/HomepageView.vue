6652ba833bf020cc6d24d60db3bfd11a476b287f
JingHai-Lingyun <m56042764@163.com>
2024-07-26 14:33:33 +0800

    UI: Optimize Color

--- src/views/Q/HomepageView.vue
+++ src/views/Q/HomepageView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useRoute } from "vue-router";
+import { ElScrollbar } from "element-plus";
 import MyLink from "@/components/MyLink.vue";
 import GithubUser from "@/components/GithubUser.vue";
 
@@ -15,12 +16,12 @@ console.log(route.query);
         </header>
         <hr />
         <main>
-            <table class="MyTable">
+            <table class="MyTable TitleTable">
                 <colgroup>
-                    <col />
-                    <col />
-                    <col />
-                    <col />
+                    <col width="25%" />
+                    <col width="25%" />
+                    <col width="25%" />
+                    <col width="25%" />
                 </colgroup>
                 <thead>
                     <th>主页名称</th>
@@ -28,109 +29,125 @@ console.log(route.query);
                     <th>联网更新地址</th>
                     <th>作者</th>
                 </thead>
-                <tr>
-                    <td>Minecraft 新闻</td>
-                    <td>
-                        <MyLink
-                            text="Light-Beacon/PCL2-NewsHomepage"
-                            url="https://github.com/Light-Beacon/PCL2-NewsHomepage"
-                            color="var(--ml-github)"
-                            type="GitHub" />
-                    </td>
-                    <td>
-                        <div class="flex">
+            </table>
+            <table class="MyTable Scroll">
+                <colgroup>
+                    <col width="25%" />
+                    <col width="25%" />
+                    <col width="25%" />
+                    <col width="25%" />
+                </colgroup>
+                    <tr>
+                        <td>Minecraft 新闻</td>
+                        <td>
                             <MyLink
-                                text="pcl.mcnews.thestack.top"
-                                url="https://pcl.mcnews.thestack.top"
-                                color="var(--ml-outlink)"
-                                type="Outlink" />
+                                text="Light-Beacon/PCL2-NewsHomepage"
+                                url="https://github.com/Light-Beacon/PCL2-NewsHomepage"
+                                color="var(--ml-github)"
+                                type="GitHub" />
+                        </td>
+                        <td>
+                            <div class="flex">
+                                <MyLink
+                                    text="pcl.mcnews.thestack.top"
+                                    url="https://pcl.mcnews.thestack.top"
+                                    color="var(--ml-outlink)"
+                                    type="Outlink" />
+                                <MyLink
+                                    text="news.bugjump.net"
+                                    url="https://news.bugjump.net"
+                                    color="var(--ml-outlink)"
+                                    type="Outlink" />
+                            </div>
+                        </td>
+                        <td>
+                            <GithubUser username="Light-Beacon" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>简单主页</td>
+                        <td>
                             <MyLink
-                                text="news.bugjump.net"
-                                url="https://news.bugjump.net"
-                                color="var(--ml-outlink)"
-                                type="Outlink" />
-                        </div>
-                    </td>
-                    <td>
-                        <GithubUser username="Light-Beacon" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>简单主页</td>
-                    <td>
-                        <MyLink
-                            text="MFn233/PCL-Mainpage"
-                            url="https://github.com/MFn233/PCL-Mainpage"
-                            color="var(--ml-github)"
-                            type="GitHub" />
-                    </td>
-                    <td>
-                        <MyLink
-                            text="MFn233/PCL-Mainpage@main/Custom.xaml"
-                            url="https://gitee.com/mfn233/PCL-Mainpage/raw/main/Custom.xaml"
-                            color="var(--ml-gitee)"
-                            type="Gitee" />
-                    </td>
-                    <td>
-                        <GithubUser username="MFn233" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>每日整合包推荐</td>
-                    <td>
-                        <MyLink
-                            text="sodamc.com"
-                            url="https://sodamc.com/31402-29042412.html"
-                            color="var(--ml-custom-1)"
-                            type="Custom">
-                            <template #image>
-                                <img src="^/images/out/sodamc.png" width="30" />
-                            </template>
-                        </MyLink>
-                    </td>
-                    <td>
-                        <MyLink
-                            text="pclsub.sodamc.com"
-                            url="https://pclsub.sodamc.com/"
-                            color="var(--ml-custom-1)"
-                            type="Custom">
-                            <template #image>
-                                <img src="^/images/out/sodamc.png" width="30" />
-                            </template>
-                        </MyLink>
-                    </td>
-                    <td>
-                        <GithubUser username="wkea" />
-                    </td>
-                </tr>
-                <tr>
-                    <td>Minecraft 皮肤推荐</td>
-                    <td>
-                        <MyLink
-                            text="sodamc.com"
-                            url="https://sodamc.com/45080-44052300.html"
-                            color="var(--ml-custom-1)"
-                            type="Custom">
-                            <template #image>
-                                <img src="^/images/out/sodamc.png" width="30" />
-                            </template>
-                        </MyLink>
-                    </td>
-                    <td>
-                        <MyLink
-                            text="forgepixel.com"
-                            url="https://forgepixel.com/pcl_sub_file"
-                            color="var(--ml-custom-1)"
-                            type="Custom">
-                            <template #image>
-                                <img src="^/images/out/forgepixel.png" width="30" />
-                            </template>
-                        </MyLink>
-                    </td>
-                    <td>
-                        <GithubUser username="wkea" />
-                    </td>
-                </tr>
+                                text="MFn233/PCL-Mainpage"
+                                url="https://github.com/MFn233/PCL-Mainpage"
+                                color="var(--ml-github)"
+                                type="GitHub" />
+                        </td>
+                        <td>
+                            <MyLink
+                                text="MFn233/PCL-Mainpage@main/Custom.xaml"
+                                url="https://gitee.com/mfn233/PCL-Mainpage/raw/main/Custom.xaml"
+                                color="var(--ml-gitee)"
+                                type="Gitee" />
+                        </td>
+                        <td>
+                            <GithubUser username="MFn233" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>每日整合包推荐</td>
+                        <td>
+                            <MyLink
+                                text="sodamc.com"
+                                url="https://sodamc.com/31402-29042412.html"
+                                color="var(--ml-custom-1)"
+                                type="Custom">
+                                <template #image>
+                                    <img
+                                        src="^/images/out/sodamc.png"
+                                        width="30" />
+                                </template>
+                            </MyLink>
+                        </td>
+                        <td>
+                            <MyLink
+                                text="pclsub.sodamc.com"
+                                url="https://pclsub.sodamc.com/"
+                                color="var(--ml-custom-1)"
+                                type="Custom">
+                                <template #image>
+                                    <img
+                                        src="^/images/out/sodamc.png"
+                                        width="30" />
+                                </template>
+                            </MyLink>
+                        </td>
+                        <td>
+                            <GithubUser username="wkea" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Minecraft 皮肤推荐</td>
+                        <td>
+                            <MyLink
+                                text="sodamc.com"
+                                url="https://sodamc.com/45080-44052300.html"
+                                color="var(--ml-custom-1)"
+                                type="Custom">
+                                <template #image>
+                                    <img
+                                        src="^/images/out/sodamc.png"
+                                        width="30" />
+                                </template>
+                            </MyLink>
+                        </td>
+                        <td>
+                            <MyLink
+                                text="forgepixel.com"
+                                url="https://forgepixel.com/pcl_sub_file"
+                                color="var(--ml-custom-1)"
+                                type="Custom">
+                                <template #image>
+                                    <img
+                                        src="^/images/out/forgepixel.png"
+                                        width="30" />
+                                </template>
+                            </MyLink>
+                        </td>
+                        <td>
+                            <GithubUser username="wkea" />
+                        </td>
+                    </tr>
             </table>
         </main>
     </div>
